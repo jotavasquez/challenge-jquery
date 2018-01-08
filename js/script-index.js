dedba8aaf5a8e47875bf0b8f50bee506de977b60
jotavasquez <jotavasquezjota@gmail.com>
2018-01-08 17:26:49 -0300

    etapa 5

--- js/script-index.js
+++ js/script-index.js
@@ -1,9 +1,9 @@
-$(document).ready( function(){
-	$("#back-arrow").remove();
+$(document).ready(function () {
+	$("#back-arrow").hide();
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	printNews();
 	renderHighlightedRecipes(recipesArray);
-	
+	renderActivities(activitiesArray);
 });
 
 //funcion etapa 2
@@ -19,10 +19,10 @@ function printNews() {
 
 function renderHighlightedRecipes(recipesArray) {
 	for (var i = 0; i < recipesArray.length; i++) {
-			if (recipesArray[i].highlighted === true){
-				//pasa la data a la función renderRecipe
-				renderRecipe(recipesArray[i]);
-			}
+		if (recipesArray[i].highlighted === true) {
+			//pasa la data a la función renderRecipe
+			renderRecipe(recipesArray[i]);
+		}
 	}
 }
 
@@ -32,46 +32,81 @@ function renderHighlightedRecipes(recipesArray) {
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
 */
-
 function renderRecipe(recipe) {
-	var title = recipe.title
 	console.log('Voy a pintar la receta: ', recipe);
-	console.log(recipe.source.name);
-	console.log(recipe.source.url);
-	var img_i ="";
 	/*Paso 4*/
 	$("#list-recipes").append(
 		'<a class="item-recipe" href="#">' +
-			'<span class="attribution">' +
-		'<span class="title-recipe">' + recipe.title + 
-				'</span>' +
-				'<span class="metadata-recipe">' +
-					'<span class="author-recipe">' + recipe.source.name + 
-					'</span>' +
-					'<span class="bookmarks-recipe">' +
-						'<span class="icon-bookmark">' +
-						'</span>' +
-					'</span>' +
-				'</span>' +
-			'</span>' +
-			'<img src= "img/recipes/320x350/'+ recipe.name + '.jpg"/>' +
+		'<span class="attribution">' +
+		'<span class="title-recipe">' + recipe.title +
+		'</span>' +
+		'<span class="metadata-recipe">' +
+		'<span class="author-recipe">' + recipe.source.name +
+		'</span>' +
+		'<span class="bookmarks-recipe">' +
+		'<span class="icon-bookmark">' +
+		'</span>' +
+		'</span>' +
+		'</span>' +
+		'</span>' +
+		'<img src= "img/recipes/320x350/' + recipe.name + '.jpg"/>' +
 		'</a>'
 	);
 }
 
+/* Paso 5 */
 /*
 * Función que se encarga de pintar todas las actividades
+* ciclo for con each
 */
+
+/*
+var activitiesArray = activities;
 function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
+	$.each(activitiesArray, function (ind, elem) {
+		if (ind > 0){
+			console.log("tiene tres" + elem);
+			renderActivity(activitiesArray);
+		} else {
+			console.log("tiene cero");
+			$('.wrapper-message').hide();
+			//o remover? 
+			//$('.wrapper-message').remove();
+		}
+	});
+}
+*/
+
+var activitiesArray = activities;
+function renderActivities(activitiesArray) {
+	if (activitiesArray.length < 0) {
+		console.log("tiene más de 0 objetos");
+		renderActivity(recipe);
+	} else {
+		console.log(" 0 objetos");
+		$('.wrapper-message').remove();
+	}
 }
 
+/*
+ $.each(activitiesArray, function (ind, elem) {
+	   if (ind >= 0){
+	   renderActivity(activitiesArray);
+	   console.log("tiene " + ind + " objetos" + elem.recipeName);
+	   } else {
+		   console.log("no hay objetos");
+		   $('.wrapper-message').hide();
+		   //o remover?
+		   //$('.wrapper-message').remove();
+	   }
+ });
+}
+/*
 /*
 * Función que se encarga de pintar una actividad
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
-	console.log('Activities: ', activitiesArray);
+	console.log(recipe);
 }
-
