aa8074d66d84c2ba0e35a02f9009ce34eaf0280b
jotavasquez <jotavasquezjota@gmail.com>
2018-01-08 19:28:27 -0300

    Etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,13 +5,21 @@ $(document).ready(function () {
 
     //Función para el boton recipe, quita la clase make
     $('.js-show-recipe').click(function () {
+        //Etapa 8
         console.log("mostrar receta");
         $('#cont').removeClass('make');
+        //Etapa 9
+        $(this).addClass('active');
+        $('.js-show-make').removeClass('active');
     })
 
     //Función para el boton make it, muestra la clase make
     $('.js-show-make').click(function () {
+        //Etapa 8
         console.log("mostrar instrucciones");
         $('#cont').addClass('make');
+        //Etapa 9
+        $(this).addClass('active');
+        $('.js-show-recipe').removeClass('active');
     })
 });
